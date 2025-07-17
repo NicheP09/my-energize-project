7c716508fe648c4353132ee732b4a1dc83d283f6
Yeboah Peter <peteryeboah09@gmail.com>
2025-07-17 19:43:29 +0100

    Made the FAQS interactive and changed the icons

--- app/main.js
+++ app/main.js
@@ -1,4 +1,10 @@
 const hamburgerBtn = document.querySelector('.hamburger-button');
+const faqsTag = document.querySelectorAll('.faqs-tag');
+
+const faqsMessage = document.querySelectorAll('.faqs-message');
+const icons = document.querySelectorAll('.fa-plus')
+
+
 
 hamburgerBtn.addEventListener('click', () => {
   const nav= document.querySelector('nav');
@@ -6,3 +12,30 @@ hamburgerBtn.addEventListener('click', () => {
   nav.classList.toggle('swipe-right');
 })
 
+let matching;
+faqsTag.forEach((Tag, index) => {
+  Tag.addEventListener('click', () => {
+     let match = index;
+   
+    
+   faqsMessage.forEach((message, index) => {
+    if (match === index ) {
+      message.classList.toggle('show')
+    }
+   })
+
+    icons.forEach((icon,index) => {
+      if (match === index) {
+         console.log(icon)
+        icon.classList.toggle('fa-minus')
+       
+      }
+    });
+
+   
+    })
+   
+  
+})
+
+ //faqsMessage.classList.toggle('show')
